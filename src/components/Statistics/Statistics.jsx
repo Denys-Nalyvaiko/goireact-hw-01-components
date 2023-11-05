4a2a858c8902e43e360abc4d7b31cecbccd1b027
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-11-05 16:18:37 +0100

    create components

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,15 @@
+import { StatisticsItem } from 'components/StatisticItem/StatisticItem';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <StatisticsItem key={id} label={label} percentage={percentage} />
+        ))}
+      </ul>
+    </section>
+  );
+};
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,26 @@
+import { TransactionItem } from 'components/TransactionItem/TransactionItem';
+
+export const TransactionHistory = ({ transactions }) => {
+  return (
+    <table className="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => (
+          <TransactionItem
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
